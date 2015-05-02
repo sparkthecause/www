10156a24bcfc6b33b2719ff92f5199c325dbff94
Charles Burgess <consulting@cvburgess.com>
2015-05-02 08:22:16 -0400

    add formatting and date to button

---
+++ assets/js/redeem.js
@@ -0,0 +1,36 @@
+$('#redeemBtn').on( 'click', function( e ) {
+
+    $.ajax({
+        "type": "POST",
+        "url": "https://api.sparkthecause.com/v0/giveday/redeem",
+        "dataType": 'json',
+        "data": {
+            "code": queryObject.code
+        }
+    });
+    e.preventDefault();
+
+});
+
+$(document).ready( function(){
+
+    queryObject = {}
+
+    if ( window.location.search.length > 1 ) {
+        for (var aItKey, nKeyId = 0, aCouples = window.location.search.substr(1).split("&"); nKeyId < aCouples.length; nKeyId++) {
+            aItKey = aCouples[nKeyId].split("=");
+            queryObject[decodeURIComponent(aItKey[0])] = aItKey.length > 1 ? decodeURIComponent(aItKey[1]) : "";
+        }
+    }
+
+    if ( moment().isBetween( '2015-05-06', '2015-05-14') ) {
+
+        $('#redeemBtn').text( "Use Coupon on " + moment().format( "MMM Do" ) );
+
+    } else {
+
+        $('.footer').hide();
+
+    }
+
+});
